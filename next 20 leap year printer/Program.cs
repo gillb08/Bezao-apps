4f3de746601c14f94a7d4d911b5155b16ef31735
Victor Gilbert Chijioke <111375035+gillb08@users.noreply.github.com>
2022-09-14 04:38:06 +0100

    Add files via upload

---
+++ next 20 leap year printer/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+ int YearX = DateTime.Now.Year;
+   int lpYr_lim = 0;
+   //int lpYr_lim = 0;
+   while (lpYr_lim < 20){
+       YearX  =  YearX + 1;
+     if(
+       (YearX % 4 == 0)){
+          lpYr_lim ++;
+          Console.WriteLine(YearX);
+       }
+   } 
